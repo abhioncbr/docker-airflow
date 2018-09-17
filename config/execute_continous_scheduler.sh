f656e6d163a9d49b29f064824a389df8b576ff1a
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2018-09-17 18:05:10 -0400

    Updated for documentation.

--- config/execute_continous_scheduler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export AIRFLOW__CORE__SQL_ALCHEMY_CONN=$1
-export AIRFLOW__CELERY__BROKER_URL=$2
-export AIRFLOW__CELERY__CELERY_RESULT_BACKEND=$2
-while [ True ]
-do
-	airflow scheduler -r 2900 > $AIRFLOW_HOME/startup_log/airflow-scheduler.log 2>&1 &
-	sleep 5m
-done
