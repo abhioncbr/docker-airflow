3731456b0034a18aa6fb1bbf2c70e3de52b97a71
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2019-02-21 20:33:12 -0500

    refactored and updated code.

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -3,7 +3,53 @@
 AIRFLOW_HOME="/usr/local/airflow"
 CMD="airflow"
 
-help(){
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+parse_args(){
+for I in "$@"
+do
+case ${I} in
+    -m=*|--mode=*)
+    MODE="${I#*=}"
+    shift # past argument=value
+    ;;
+    -t=*|--node_type=*)
+    NODE_TYPE="${I#*=}"
+    shift # past argument=value
+    ;;
+    -d=*|--mysql_url=*)
+    MYSQL_URL="${I#*=}"
+    shift # past argument=value
+    ;;
+    -r=*|--redis_url=*)
+    REDIS_URL="${I#*=}"
+    shift # past argument=value
+    ;;
+    -a=*|--rbac_auth=*)
+    RBAC_AUTH="${I#*=}"
+    shift # past argument=value
+    ;;
+    -s=*|--s3_path=*)
+    S3_PATH="${I#*=}"
+    shift # past argument=value
+    ;;
+    -p=*|--gcp_project=*)
+    GCP_PROJECT="${I#*=}"
+    shift # past argument=value
+    ;;
+    -u=*|--gcp_service_user=*)
+    GCP_USER_NAME="${I#*=}"
+    shift # past argument=value
+    ;;
+    *)
+    echo "Unknown option: '${I}'"
+    help
+    exit -1
+    ;;
+esac
+done
+}
+
+help_note(){
   echo
   echo "###########################################################################################################################################################################"
   echo "##***************************************************** Please provide required arguments as per below information. *****************************************************##"
@@ -37,93 +83,47 @@ print(){
   echo
 }
 
-# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
-parse_args(){
-for i in "$@"
-do
-case $i in
-    -m=*|--mode=*)
-    MODE="${i#*=}"
-    shift # past argument=value
-    ;;
-    -t=*|--node_type=*)
-    NODE_TYPE="${i#*=}"
-    shift # past argument=value
-    ;;
-    -d=*|--mysql_url=*)
-    MYSQL_URL="${i#*=}"
-    shift # past argument=value
-    ;;
-    -r=*|--redis_url=*)
-    REDIS_URL="${i#*=}"
-    shift # past argument=value
-    ;;
-    -a=*|--rbac_auth=*)
-    RBAC_AUTH="${i#*=}"
-    shift # past argument=value
-    ;;
-    -s=*|--s3_path=*)
-    S3_PATH="${i#*=}"
-    shift # past argument=value
-    ;;
-    -p=*|--gcp_project=*)
-    GCP_PROJECT="${i#*=}"
-    shift # past argument=value
-    ;;
-    -u=*|--gcp_service_user=*)
-    GCP_USER_NAME="${i#*=}"
-    shift # past argument=value
-    ;;
-    *)
-    echo "Unknown option: '${i}'"
-    help
-    exit -1
-    ;;
-esac
-done
-}
-
 print_mysql_url(){
-       if [ -v MYSQL_URL ] && [ "$MODE" = "standalone" ]; then
+       if [[ -v MYSQL_URL ]] && [[ "$MODE" = "standalone" ]]; then
        temp="  [Ignoring, since 'mode' is 'standalone']"
        fi
        echo "MYSQL_URL = ${MYSQL_URL} $temp"
 }
 
 print_node_type(){
-       if [ -v NODE_TYPE ] &&  [ "$MODE" = "standalone" ]; then
+       if [[ -v NODE_TYPE ]] &&  [[ "$MODE" = "standalone" ]]; then
        temp="  [Ignoring, since 'mode' is 'standalone']"
        fi
        echo "NODE_TYPE = ${NODE_TYPE} $temp"
 }
 
 print_redis_url(){
-       if [ -v REDIS_URL ] && [ "$MODE" = "standalone" ]; then
+       if [[ -v REDIS_URL ]] && [[ "$MODE" = "standalone" ]]; then
        temp="[Ignoring, since 'mode' is 'standalone']"
-       elif [ -v REDIS_URL ] && [ "$NODE_TYPE" = "server" ]; then
+       elif [[ -v REDIS_URL ]] && [[ "$NODE_TYPE" = "server" ]]; then
        temp="[Ignoring, since 'node_type' is 'server']"
        fi
        echo "REDIS_URL = ${REDIS_URL}  $temp"
 }
 
 validate_args(){
-    if [ "$MODE" != "cluster" ] && [ "$MODE" != "standalone" ]; then
+    if [[ "$MODE" != "cluster" ]] && [[ "$MODE" != "standalone" ]]; then
         echo "Unknown Mode: '${MODE}'"
         help
         exit -1
-    elif ([ "$MODE" = "cluster" ] && [ "$NODE_TYPE" != "server" ] && [ "$NODE_TYPE" != "worker" ]); then
+    elif ([[ "$MODE" = "cluster" ]] && [[ "$NODE_TYPE" != "server" ]] && [[ "$NODE_TYPE" != "worker" ]]); then
         echo "Unknown node_type: '${NODE_TYPE}'"
         help
         exit -1
-    elif ([ "$MODE" = "cluster" ] && [[ "$MYSQL_URL" != mysql://* ]]); then
+    elif ([[ "$MODE" = "cluster" ]] && [[ "$MYSQL_URL" != mysql://* ]]); then
         echo "Unknown mysql_url: '${MYSQL_URL}'"
         help
         exit -1
-    elif ([ "$MODE" = "cluster" ] && [ "$NODE_TYPE" = "worker" ] && [[ "$REDIS_URL" != redis://* ]]); then
+    elif ([[ "$MODE" = "cluster" ]] && [[ "$NODE_TYPE" = "worker" ]] && [[ "$REDIS_URL" != redis://* ]]); then
         echo "Unknown REDIS_URL: '${REDIS_URL}'"
         help
         exit -1
-    elif [ -v RBAC_AUTH ] && [ "$RBAC_AUTH" != "true" ] && [ "$RBAC_AUTH" != "false" ]; then
+    elif [[ -v RBAC_AUTH ]] && [[ "$RBAC_AUTH" != "true" ]] && [[ "$RBAC_AUTH" != "false" ]]; then
         echo "Unknown RBAC_AUTH: '${RBAC_AUTH}'"
         help
         exit -1
@@ -135,21 +135,21 @@ validate_args(){
 set_airflow_s3_params(){
     echo "setting s3 log directory ..."
     echo "Setting AIRFLOW__CORE__S3_LOG_FOLDER=${S3_PATH}"
-    export AIRFLOW__CORE__S3_LOG_FOLDER=$S3_PATH
-    echo "export AIRFLOW__CORE__S3_LOG_FOLDER="$S3_PATH>>~/.bashrc
-    echo "AIRFLOW__CORE__S3_LOG_FOLDER="$S3_PATH>>~/.profile
+    export AIRFLOW__CORE__S3_LOG_FOLDER=${S3_PATH}
+    echo "export AIRFLOW__CORE__S3_LOG_FOLDER="${S3_PATH}>>~/.bashrc
+    echo "AIRFLOW__CORE__S3_LOG_FOLDER="${S3_PATH}>>~/.profile
 
     S3_LOGGING_CLASS='airflow.config_templates.s3_logger.LOGGING_CONFIG'
-    AIRFLOW__CORE__LOGGING_CONFIG_CLASS=$S3_LOGGING_CLASS
-    export AIRFLOW__CORE__LOGGING_CONFIG_CLASS=$S3_TASK
-    echo "export AIRFLOW__CORE__LOGGING_CONFIG_CLASS="$S3_TASK>>~/.bashrc
-    echo "AIRFLOW__CORE__LOGGING_CONFIG_CLASS="$S3_TASK>>~/.profile
+    AIRFLOW__CORE__LOGGING_CONFIG_CLASS=${S3_LOGGING_CLASS}
+    export AIRFLOW__CORE__LOGGING_CONFIG_CLASS=${S3_LOGGING_CLASS}
+    echo "export AIRFLOW__CORE__LOGGING_CONFIG_CLASS="${S3_LOGGING_CLASS}>>~/.bashrc
+    echo "AIRFLOW__CORE__LOGGING_CONFIG_CLASS="${S3_LOGGING_CLASS}>>~/.profile
 
     S3_TASK='s3.task'
-    AIRFLOW__CORE__TASK_LOG_READER=$S3_TASK
-    export AIRFLOW__CORE__S3_LOG_FOLDER=$S3_TASK
-    echo "export AIRFLOW__CORE__TASK_LOG_READER="$S3_TASK>>~/.bashrc
-    echo "AIRFLOW__CORE__TASK_LOG_READER="$S3_TASK>>~/.profile
+    AIRFLOW__CORE__TASK_LOG_READER=${S3_TASK}
+    export AIRFLOW__CORE__S3_LOG_FOLDER=${S3_TASK}
+    echo "export AIRFLOW__CORE__TASK_LOG_READER="${S3_TASK}>>~/.bashrc
+    echo "AIRFLOW__CORE__TASK_LOG_READER="${S3_TASK}>>~/.profile
 }
 
 set_gcp_params(){
@@ -175,27 +175,27 @@ set_mysql(){
 
     echo "setting mysql database connection string ..."
     echo "Setting AIRFLOW__CORE__SQL_ALCHEMY_CONN=${MYSQL_URL}"
-    export AIRFLOW__CORE__SQL_ALCHEMY_CONN=$MYSQL_URL
+    export AIRFLOW__CORE__SQL_ALCHEMY_CONN=${MYSQL_URL}
 
-    echo "export AIRFLOW__CORE__SQL_ALCHEMY_CONN="$MYSQL_URL>>~/.bashrc
-    echo "AIRFLOW__CORE__SQL_ALCHEMY_CONN="$MYSQL_URL>>~/.profile
+    echo "export AIRFLOW__CORE__SQL_ALCHEMY_CONN="${MYSQL_URL}>>~/.bashrc
+    echo "AIRFLOW__CORE__SQL_ALCHEMY_CONN="${MYSQL_URL}>>~/.profile
 }
 
 set_celery_redis(){
 	echo "Setting AIRFLOW__CELERY__BROKER_URL=${REDIS_URL}"
-    export AIRFLOW__CELERY__BROKER_URL=$REDIS_URL
+    export AIRFLOW__CELERY__BROKER_URL=${REDIS_URL}
     echo "Setting AIRFLOW__CELERY__CELERY_RESULT_BACKEND=${REDIS_URL}"
-    export AIRFLOW__CELERY__CELERY_RESULT_BACKEND=$REDIS_URL
+    export AIRFLOW__CELERY__CELERY_RESULT_BACKEND=${REDIS_URL}
 
-    echo "export AIRFLOW__CELERY__BROKER_URL="$REDIS_URL>>~/.bashrc
-    echo "AIRFLOW__CELERY__BROKER_URL="$REDIS_URL>>~/.profile
-	echo "export AIRFLOW__CELERY__CELERY_RESULT_BACKEND="$REDIS_URL>>~/.bashrc
-    echo "AIRFLOW__CELERY__CELERY_RESULT_BACKEND="$REDIS_URL>>~/.profile
+    echo "export AIRFLOW__CELERY__BROKER_URL="${REDIS_URL}>>~/.bashrc
+    echo "AIRFLOW__CELERY__BROKER_URL="${REDIS_URL}>>~/.profile
+	echo "export AIRFLOW__CELERY__CELERY_RESULT_BACKEND="${REDIS_URL}>>~/.bashrc
+    echo "AIRFLOW__CELERY__CELERY_RESULT_BACKEND="${REDIS_URL}>>~/.profile
 }
 
 #only support password based or rbac based
 set_web_authentication(){
-    if [ "$RBAC_AUTH" = "true" ]; then
+    if [[ "$RBAC_AUTH" = "true" ]]; then
         echo "setting 'rbac web-authentication' of airflow webserver..."
         echo "Setting AIRFLOW__WEBSERVER__RBAC=True"
         export AIRFLOW__WEBSERVER__RBAC=True
@@ -211,26 +211,20 @@ set_web_authentication(){
 
 set_optional_param(){
     set_web_authentication
-    if [ -v S3_PATH ]; then
+    if [[ -v S3_PATH ]]; then
       set_airflow_s3_params
     fi
 
-    if [ -v GCP_PROJECT ] && [ -v GCP_USER_NAME ]; then
+    if [[ -v GCP_PROJECT ]] && [[ -v GCP_USER_NAME ]]; then
       set_gcp_params
     fi
 }
 
-parse_args "$@"
-validate_args
-
-# Starting airflow container in standalone mode.
-# Steps are : initialising airflow database, starting airflow scheduler & airflow webserver.
-if [ "$MODE" = "standalone" ]; then
-    set_optional_param
-
+#intialize airflow metadata db & create admin user
+set_airflow_metadataDB(){
 	# Initialising airflow database.
     echo "initialising airfow db"
-	$CMD initdb
+	${CMD} initdb
 	sleep 2
     echo "Done with airfow db"
 
@@ -239,13 +233,25 @@ if [ "$MODE" = "standalone" ]; then
     sleep 1
     python user_add.py
 
-    echo "Running ab_user_add python script in-case 'airflow' rbac_user is not present. Password is: 'airflow'"
-    sleep 1
-    #python ab_user_add.py
+    if [[ "$RBAC_AUTH" = "true" ]]; then
+        echo "Running rbac_user_add python script in-case 'airflow' rbac_user is not present. Password is: 'airflow'"
+        sleep 1
+        python rbac_user_add.py
+    fi
+}
+
+parse_args "$@"
+validate_args
+
+# Starting airflow container in standalone mode.
+# Steps are : initialising airflow database, starting airflow scheduler & airflow webserver.
+if [[ "$MODE" = "standalone" ]]; then
+    set_optional_param
+    set_airflow_metadataDB
 
 	# Starting airflow scheduler and writing scheduler log in file 'startup_log/airflow-scheduler.log'.
 	echo starting airflow scheduler
-	exec -a airflow-scheduler $CMD scheduler > $AIRFLOW_HOME/startup_log/airflow-scheduler.log 2>&1 &
+	exec -a airflow-scheduler ${CMD} scheduler > ${AIRFLOW_HOME}/startup_log/airflow-scheduler.log 2>&1 &
 	sleep 5
 	case "$(pidof /usr/local/bin/python /usr/local/bin/airflow scheduler | wc -w)" in
 		0)  echo "airflow scheduler is not started .. exiting."
@@ -255,22 +261,23 @@ if [ "$MODE" = "standalone" ]; then
     		;;
 	esac
 
-	# Starting airflow webserver and writing log in to the file 'startup_log/airflow-server.log'.
-	echo "starting airflow webserver"
-	exec -a airflow-webserver $CMD webserver -p 2222 > $AIRFLOW_HOME/startup_log/airflow-server.log 2>&1
+	# Starting airflow web-server and writing log in to the file 'startup_log/airflow-server.log'.
+	echo "starting airflow web-server"
+	exec -a airflow-webserver ${CMD} webserver -p 2222 > ${AIRFLOW_HOME}/startup_log/airflow-server.log 2>&1
 
 # Starting airflow server.
 # Steps are : initialising airflow database, starting redis server, starting airflow scheduler, starting airflow webserver
-elif [ "$MODE" = "cluster" ] && [ "$NODE_TYPE" = "server" ]; then
+elif [[ "$MODE" = "cluster" ]] && [[ "$NODE_TYPE" = "server" ]]; then
    set_mysql
    set_celery_executor
    set_optional_param
+   set_airflow_metadataDB
 
-# ============= Starting server processes =====================
+    # ============= Starting server processes =====================
 
 	# Starting redis first as redis connection string is required for airflow.
 	echo starting redis
-	exec -a redis-server redis-server --protected-mode no > $AIRFLOW_HOME/startup_log/redis-server.log 2>&1 &
+	exec -a redis-server redis-server --protected-mode no > ${AIRFLOW_HOME}/startup_log/redis-server.log 2>&1 &
 	sleep 5
 	case "$(pidof redis-server | wc -w)" in
 		0)  echo "redis is not started .. exiting."
@@ -283,28 +290,9 @@ elif [ "$MODE" = "cluster" ] && [ "$NODE_TYPE" = "server" ]; then
 	REDIS_URL=redis://localhost:6379/0
 	set_celery_redis
 
-	# Initialising airflow database.
-	echo "initialising airfow db"
-	$CMD initdb
-	if [ "$?" -ne 0 ]; then
-		echo "airflow initdb command is not successful. Exiting."
-		exit 1
-	fi
-	sleep 2
-    echo "Done with airfow db"
-
-	#executing python script for adding user in-case if user is not present
-	echo "Running user_add python script in-case 'airflow' user is not present. Password is: 'airflow'"
-	sleep 1
-	python user_add.py
-
-	echo "Running ab_user_add python script in-case 'airflow' rbac_user is not present. Password is: 'airflow'"
-	sleep 1
-    #python ab_user_add.py
-
 	# Starting airflow scheduler and writing scheduler log in file 'startup_log/airflow-scheduler.log'.
 	echo starting airflow scheduler
-	exec -a airflow-scheduler $CMD scheduler > $AIRFLOW_HOME/startup_log/airflow-scheduler.log 2>&1 &
+	exec -a airflow-scheduler ${CMD} scheduler > ${AIRFLOW_HOME}/startup_log/airflow-scheduler.log 2>&1 &
 	sleep 5
 	case "$(pidof /usr/local/bin/python/usr/local/bin/airflow scheduler | wc -w)" in
 		0)  echo "airflow scheduler is not started .. exiting."
@@ -316,14 +304,14 @@ elif [ "$MODE" = "cluster" ] && [ "$NODE_TYPE" = "server" ]; then
 
 	#running shell script to restart airflow scheduler in every 5 minutes.
 	echo "Running shell script to restart airflow scheduler in every 5 minutes."
-	sh ./execute_continous_scheduler.sh $MYSQL_CONNECTION $REDIS_CONNECTION &
+	sh ./execute_continous_scheduler.sh ${MYSQL_URL} ${REDIS_URL} &
 
-	# Starting airflow webserver and writing log in to the file 'startup_log/airflow-server.log'.
-	echo "starting airflow webserver"
-	exec -a airflow-webserver $CMD webserver > $AIRFLOW_HOME/startup_log/airflow-server.log 2>&1
+	# Starting airflow web-server and writing log in to the file 'startup_log/airflow-server.log'.
+	echo "starting airflow web-server"
+	exec -a airflow-webserver ${CMD} webserver > ${AIRFLOW_HOME}/startup_log/airflow-server.log 2>&1
 
 # Starting airflow worker.
-elif [ "$MODE" = "cluster" ] && [ "$NODE_TYPE" = "worker" ]; then
+elif [[ "$MODE" = "cluster" ]] && [[ "$NODE_TYPE" = "worker" ]]; then
    set_mysql
    set_celery_executor
    set_celery_redis
@@ -331,7 +319,7 @@ elif [ "$MODE" = "cluster" ] && [ "$NODE_TYPE" = "worker" ]; then
 
 	# Starting worker processes.
 	echo "starting airflow celery flower"
-    exec $CMD flower > $AIRFLOW_HOME/startup_log/airflow-celery-flower.log 2>&1 &
+    exec ${CMD} flower > ${AIRFLOW_HOME}/startup_log/airflow-celery-flower.log 2>&1 &
     sleep 5
     case "$(pidof /usr/local/bin/python/usr/local/bin/flower | wc -w)" in
 		0)  echo "airflow flower is not started .. exiting."
@@ -343,9 +331,9 @@ elif [ "$MODE" = "cluster" ] && [ "$NODE_TYPE" = "worker" ]; then
 
 	echo "starting airflow worker"
 	QUEUE="default,$(hostname)"
-	exec $CMD worker -q ${QUEUE} > $AIRFLOW_HOME/startup_log/airflow-worker.log 2>&1
+	exec ${CMD} worker -q ${QUEUE} > ${AIRFLOW_HOME}/startup_log/airflow-worker.log 2>&1
 
 # arguments is not in order
 else
-help
+help_note
 fi
